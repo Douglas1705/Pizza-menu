8905e3a474fb49ca8e103b688c476ab1d42692dc
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-09-21 20:25:29 -0300

    creating header, footer, menu, globalStyles folders

---
+++ src/layoutPage/header/header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+        <h1>Ola mundo</h1>
+    )
+}
+
+export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import GlobalStyle from './globalStyle/globalStyle.js'
+import Header from './layoutPage/header/header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    
+    <Header />
+    <GlobalStyle />
   </StrictMode>,
+  
 )
